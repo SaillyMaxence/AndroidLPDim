e385e28891da2ecd61e885451620fe391fcfebb5
Florian <fournier.florian.iut@gmail.com>
2019-03-12 17:28:49 +0100

    Restriction de l'affichage
    Affiche seulement les ligues de foot.

--- app/src/main/java/com/example/lpdimandroid/Home.java
+++ app/src/main/java/com/example/lpdimandroid/Home.java
@@ -46,16 +46,17 @@ public class Home extends AppCompatActivity {
 
                 for (final League league : leagues.getLeagues()) {
                     final String idLeague = league.getIdLeague();
-                    Button myButton = new Button(Home.this);
-                    myButton.setText(league.getStrLeague());
+                    if (league.getStrSport().equals("Soccer")) {
+                        Button myButton = new Button(Home.this);
+                        myButton.setText(league.getStrLeague());
 
-                    Home.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            myLayout.addView(myButton);
-                        }
-                    });
-
+                        Home.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                myLayout.addView(myButton);
+                            }
+                        });
+                    }
                 }
 
             }
