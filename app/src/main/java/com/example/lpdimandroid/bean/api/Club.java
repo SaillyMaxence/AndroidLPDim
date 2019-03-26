3302bb39b4910488031e284ce22db7e08d6c6418
Florian <fournier.florian.iut@gmail.com>
2019-03-26 15:23:56 +0100

    ADD club et modification League

---
+++ app/src/main/java/com/example/lpdimandroid/bean/api/Club.java
@@ -0,0 +1,8 @@
+package com.example.lpdimandroid.bean.api;
+
+/**
+ * Created by florian on 26/03/2019.
+ */
+
+public class Club {
+}
--- app/src/main/java/com/example/lpdimandroid/bean/api/ClubByLeague.java
+++ app/src/main/java/com/example/lpdimandroid/bean/api/ClubByLeague.java
@@ -6,4 +6,19 @@ import java.util.List;
 
 
 public class ClubByLeague {
+
+    private List<Club> clubs;
+
+    public SearchClubs() {
+        setClubs(new ArrayList<League>());
+    }
+
+    public List<League> getCountrys() {
+        return clubs;
+    }
+
+    public void setClubs(List<Club> clubs) {
+        this.clubs = clubs;
+    }
+
 }
