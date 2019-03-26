0def021a09833d066e36da1704c0509b5d051303
maxence.sailly <maxencesai@gmail.com>
2019-03-26 14:50:12 +0100

    add event on match and club button

--- app/src/main/java/com/example/lpdimandroid/LeagueActivity.java
+++ app/src/main/java/com/example/lpdimandroid/LeagueActivity.java
@@ -2,6 +2,8 @@ package com.example.lpdimandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LeagueActivity extends AppCompatActivity {
 
@@ -10,6 +12,22 @@ public class LeagueActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_league);
 
+        Button club = findViewById(R.id.club);
+
+        club.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // club de la league
+            }
+        });
+
+        Button dayMatch = findViewById(R.id.dayMatch);
+        dayMatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Afficher les match du jour
+            }
+        });
 
     }
 }
